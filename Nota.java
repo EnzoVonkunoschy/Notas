c0173a2cc0f1f768c52c487510e43e5d5d2ed943
willydevelop <willydev365@gmail.com>
2026-06-11 01:00:17 -0300

    Clases e interfaces creadas

---
+++ Nota.java
@@ -0,0 +1,33 @@
+public class Nota {
+    private Destinatario destinatario;
+    private Empleado autor;
+    private String texto;
+
+    public Destinatario getDestinatario(){
+        return destinatario;
+    }
+    public void setDestinatario(Destinatario destinatario){
+        this.destinatario = destinatario;
+    }
+
+    public Empleado getAutor(){
+        return autor;
+    }
+
+    public void setAutor(Empleado autor){
+        this.autor = autor;
+    }
+
+    public String getTexto(){
+        return texto;
+    }
+
+    public void setTexto(String texto){
+        this.texto = texto;
+    }
+
+    @Override
+    public String toString(){
+        return " ||"+" Notas: " + this.texto + " -- Autor: "+ this.autor + " -- Destinatario: "+this.destinatario;
+    }
+}
---
+++ Oficina.java
@@ -0,0 +1,17 @@
+public class Oficina implements Destinatario {
+    public String nombre = " ";
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+
+}
